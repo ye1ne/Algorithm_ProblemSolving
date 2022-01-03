d1ff8563b2055902322b92aba90342f1ee80c73e
Yewon Jeong <yewon7036@gmail.com>
2022-01-03 15:34:23 +0900

    220103

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        int a = 10000;
-
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.printf("%d",a*(b-1));
 
     }
 }
