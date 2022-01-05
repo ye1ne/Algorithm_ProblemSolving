8d744306f2a56ca98e179468b77e3b0708c313d3
Yewon Jeong <yewon7036@gmail.com>
2022-01-05 15:49:23 +0900

    220103

---
+++ src/Bronze/B1546.java
@@ -0,0 +1,31 @@
+package Bronze;
+
+
+import java.util.Scanner;
+
+public class B1546 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int subjectNum = scanner.nextInt();
+
+        int[] scoreArray = new int[subjectNum];
+
+        for(int i =0; i<subjectNum; i++){
+            scoreArray[i] = scanner.nextInt();
+        }
+        //System.out.println(Arrays.toString(scoreArray));
+        int max = scoreArray[0];
+        for(int i=0; i<subjectNum; i++){
+            if(scoreArray[i]>max) max=scoreArray[i];
+        }
+
+        double tmp;
+        double sum = 0;
+        for(int i=0; i<subjectNum; i++){
+            tmp = (scoreArray[i]/(double)max)*100;
+            sum = sum + tmp;
+        }
+        System.out.printf("%.14f",sum/subjectNum);
+
+    }
+}
---
+++ src/Silver/S1094.java
@@ -0,0 +1,30 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class S1094 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt(); //숫자를 문자열로 입력받아서
+
+        int[] numArray = new int[num];
+        for(int i =0; i<num; i++){ //input 문자열 길이만큼 도는데
+            numArray[i] = scanner.nextInt();
+        }
+        int tmp;
+        for(int i=0; i< num-1; i++){
+            for(int j=0; j<num-1-i; j++){
+                if(numArray[j]>numArray[j+1]){
+                    tmp = numArray[j];
+                    numArray[j] = numArray[j+1];
+                    numArray[j+1] = tmp;
+                }
+
+            }
+        }
+
+        for(int i=0; i< num; i++){
+            System.out.println(numArray[i]);
+        }
+    }
+}
