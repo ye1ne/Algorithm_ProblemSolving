966177135caefeed9092df1f4d76a5d1cad68a0d
ye1ne <yewon7036@naver.com>
2022-09-04 13:05:24 +0900

    LeetCode String 344 revers String

---
+++ src/leetcode/A2_344_문자열뒤집기.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class A2_344_문자열뒤집기 {
+    public static void main(String[] args) {
+        char[] s={'h','e','l','l','o'};
+
+        int length = s.length%2 == 0 ? s.length/2 : s.length/2 +1;
+        for(int i =0; i< length; i++){
+            char tmp = s[i];
+            s[i] = s[s.length-i-1];
+            s[s.length-i-1] = tmp;
+        }
+
+        System.out.println(Arrays.toString(s));
+    }
+}
