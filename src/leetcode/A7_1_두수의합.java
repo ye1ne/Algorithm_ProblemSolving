30b552b6d1cbd5417888dd154d800b9f4d5b8b81
ye1ne <yewon7036@naver.com>
2022-09-06 14:41:22 +0900

    Leetcode Array 1 Two Some

---
+++ src/leetcode/A7_1_두수의합.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+
+public class A7_1_두수의합 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] arr ;
+
+        for(int i=0;i<nums.length;i++) for(int j=i+1;j<nums.length;j++)if(nums[i]+nums[j] == target)arr = new int[]{i,j};
+
+
+    }
+}
