0255ff6b85f4af7a1c29258aa4ac3e4e912bebba
ye1ne <yewon7036@naver.com>
2022-09-07 16:59:25 +0900

    Leetcode LinkedList 234 palindrome linked list

---
+++ src/BOJ_Study/BJ_STR/S11655.java
@@ -0,0 +1,24 @@
+package BOJ_Study.BJ_STR;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S11655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str =br.readLine();
+
+        for(int i=0; i<str.length()/2;i++){
+            if(str.charAt(i) == str.charAt(str.length()-1-i)) continue;
+            else {
+                System.out.println(0);
+                return;
+            }
+        }
+        System.out.println(1);
+
+        return;
+
+    }
+}
---
+++ src/leetcode/A13_234_팰린드롬연결리스트.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import javax.print.DocFlavor;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.Stack;
+
+public class A13_234_팰린드롬연결리스트 {
+     static class ListNode {
+         int val;
+         ListNode next;
+         public ListNode() {}
+         public ListNode(int val) { this.val = val; }
+         public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public static void main(String[] args) {
+
+
+        ListNode head = new ListNode(1,new ListNode(2,new ListNode(2,new ListNode(1))));
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        while(head.next != null){
+            deque.add(head.val);
+            head = head.next;
+
+            if(head.next == null) deque.add(head.val);
+        }
+
+        while(!deque.isEmpty()){
+            int front = deque.pollFirst();
+            if(deque.isEmpty()) continue;
+            int end = deque.pollLast();
+            if(front != end) System.out.println("아님");
+        }
+
+        System.out.println("마즁");
+   }
+}
