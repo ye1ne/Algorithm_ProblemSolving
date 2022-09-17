fa8d74aea2037a25e41dec22a16ee788c11ec5f2
ye1ne <yewon7036@naver.com>
2022-09-17 14:18:59 +0900

    Leetcode tree 617 두이진트리병합

--- src/leetcode/ch4_비성형자료구조/A45_226_이진트리반전.java
+++ src/leetcode/ch4_비성형자료구조/A45_226_이진트리반전.java
@@ -1,2 +1,38 @@
-package leetcode.ch4_비성형자료구조;public class A45_226_이진트리반전 {
+package leetcode.ch4_비성형자료구조;
+
+public class A45_226_이진트리반전 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(1,new TreeNode(4,new TreeNode(4),new TreeNode(4)),new TreeNode(5,null,new TreeNode(5)));
+
+        TreeNode root = new TreeNode(4,new TreeNode(2,new TreeNode(1),new TreeNode(3)),new TreeNode(7,new TreeNode(6),new TreeNode(9)));
+
+        root = dfs(root);
+        System.out.println();
+        
+    }
+
+    static TreeNode dfs(TreeNode node){
+        if(node == null) return null;
+
+        TreeNode left = dfs(node.left);
+        TreeNode right = dfs(node.right);
+
+        node.left = right;
+        node.right = left;
+        return node;
+    }
 }
---
+++ src/leetcode/ch4_비성형자료구조/A46_617_두이진트리병합.java
@@ -0,0 +1,50 @@
+package leetcode.ch4_비성형자료구조;
+
+public class A46_617_두이진트리병합 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+//        TreeNode root = new TreeNode(1,new TreeNode(4,new TreeNode(4),new TreeNode(4)),new TreeNode(5,null,new TreeNode(5)));
+
+        TreeNode root1 = new TreeNode(1,new TreeNode(3,new TreeNode(5),null),new TreeNode(2,null,null));
+        TreeNode root2 = new TreeNode(2,new TreeNode(1,null,new TreeNode(4)),new TreeNode(3,null,new TreeNode(7)));
+
+
+        TreeNode node = dfs(root1,root2);
+
+        System.out.println();
+
+
+    }
+
+    static TreeNode dfs(TreeNode node1,TreeNode node2){
+
+        if(node1 == null && node2 == null){
+            return  null;
+        }
+        if(node1 == null){
+            return node2;
+        }
+        if(node2 == null){
+            return node1;
+        }
+
+        TreeNode left = dfs(node1.left,node2.left);
+        TreeNode right = dfs(node1.right,node2.right);
+
+        TreeNode node = new TreeNode(node1.val+node2.val,left,right);
+        return node;
+    }
+}
